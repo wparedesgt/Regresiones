57b2d293bf033c60c9343263615318c6f04177ab
wparedesgt <wparedesgt@gmail.com>
2018-09-18 19:56:51 -0600

    falling

--- HC_Measurement_Error_Models.R
+++ HC_Measurement_Error_Models.R
@@ -1,95 +1,109 @@
 #Modelos de error de medición
 
-Hasta ahora, todos nuestros ejemplos de regresión lineal se han aplicado a dos o más variables aleatorias.
+#Hasta ahora, todos nuestros ejemplos de regresión lineal se han aplicado a dos o más variables aleatorias.
 
-Suponemos que los pares son bivariados normales y usamos esto para motivar un modelo lineal.
+#Suponemos que los pares son bivariados normales y usamos esto para motivar un modelo lineal.
 
-Este enfoque cubre la mayoría de los ejemplos de la vida real donde se usa la regresión lineal.
+#Este enfoque cubre la mayoría de los ejemplos de la vida real donde se usa la regresión lineal.
 
-La otra aplicación principal proviene de modelos de error de medición.
+#La otra aplicación principal proviene de modelos de error de medición.
 
-En estas aplicaciones, es común tener covariables no aleatorias, como el tiempo.
+#En estas aplicaciones, es común tener covariables no aleatorias, como el tiempo.
 
-Y la aleatoriedad se introduce a partir del error de medición, en lugar del muestreo o la variabilidad natural.
+#Y la aleatoriedad se introduce a partir del error de medición, en lugar del muestreo o la variabilidad natural.
 
-Para comprender estos modelos, vamos a utilizar un ejemplo de motivación relacionado con la física.
+#Para comprender estos modelos, vamos a utilizar un ejemplo de motivación relacionado con la física.
 
-Imagina que eres Galileo en el siglo XVI tratando de describir la velocidad de un objeto que cae.
+#Imagina que eres Galileo en el siglo XVI tratando de describir la velocidad de un objeto que cae.
 
-Un asistente sube la Torre de Pisa y deja caer una pelota.
+#Un asistente sube la Torre de Pisa y deja caer una pelota.
 
-Mientras que muchos otros asistentes registran la posición en diferentes momentos.
+#Mientras que muchos otros asistentes registran la posición en diferentes momentos.
 
-El conjunto de datos del objeto que cae contiene un ejemplo de cómo se verían esos datos.
+#El conjunto de datos del objeto que cae contiene un ejemplo de cómo se verían esos datos.
 
-El asistente entrega los datos a Galileo y esto es lo que ve.
+library(tidyverse)
+library(dslabs)
 
-Él usa ggplot para hacer una trama.
 
-Aquí vemos la distancia en metros que ha caído en el eje y y el tiempo en el eje x.
+falling_object <- rfalling_object()
 
-Galileo no conoce la ecuación exacta, pero a partir de la exploración de datos, al observar la trama, deduce que la posición debe seguir una parábola, que podemos escribir así.
 
-Los datos no caen exactamente en una parábola, pero Galileo sabe que esto se debe a un error de medición.
+#El asistente entrega los datos a Galileo y esto es lo que ve.
 
-Sus ayudantes se equivocan al medir la distancia que ha caído la pelota.
+#Él usa ggplot para hacer una trama.
 
-Para dar cuenta de esto, escribimos este modelo.
+falling_object %>% 
+  ggplot(aes(time, observed_distance)) +
+  geom_point() +
+  ylab("Distancia en Metros") + 
+  xlab("Tiempo en Segundos")
 
-Aquí, y representa la distancia que la bola se deja caer en metros.
 
-Xi representa el tiempo en segundos.
+#Aquí vemos la distancia en metros que ha caído en el eje y y el tiempo en el eje x.
 
-Y epsilon representa un error de medición.
+#Galileo no conoce la ecuación exacta, pero a partir de la exploración de datos, al observar la trama, deduce que la posición debe seguir una parábola, que podemos escribir así.
 
-Se supone que el error de medición es aleatorio, independiente entre sí y que tiene la misma distribución de cada ojo.
+#Los datos no caen exactamente en una parábola, pero Galileo sabe que esto se debe a un error de medición.
 
-También suponemos que no hay sesgo, lo que significa que el valor esperado de épsilon es 0.
+#Sus ayudantes se equivocan al medir la distancia que ha caído la pelota.
 
-Tenga en cuenta que este es un modelo lineal porque es una combinación lineal de cantidades conocidas.
+#Para dar cuenta de esto, escribimos este modelo.
 
-X y x al cuadrado son parámetros conocidos y desconocidos, los betas.
+#Aquí, y representa la distancia que la bola se deja caer en metros.
 
-A diferencia de nuestro ejemplo anterior, las x son cantidades fijas.
+#Xi representa el tiempo en segundos.
 
-Esto es solo tiempo.
+#Y epsilon representa un error de medición.
 
-No estamos condicionando.
+#Se supone que el error de medición es aleatorio, independiente entre sí y que tiene la misma distribución de cada ojo.
 
-Ahora para plantear una nueva teoría física y comenzar a hacer predicciones sobre otros objetos que caen, Galileo necesita números reales, en lugar de los parámetros desconocidos.
+#También suponemos que no hay sesgo, lo que significa que el valor esperado de épsilon es 0.
 
-Las estimaciones de LSE squares parecen un enfoque razonable.
+#Tenga en cuenta que este es un modelo lineal porque es una combinación lineal de cantidades conocidas.
 
-Entonces, ¿cómo encontramos las estimaciones de cuadrados LSE?
+#X y x al cuadrado son parámetros conocidos y desconocidos, los betas.
+
+#A diferencia de nuestro ejemplo anterior, las x son cantidades fijas.
+
+#Esto es solo tiempo.
+
+#No estamos condicionando.
+
+#Ahora para plantear una nueva teoría física y comenzar a hacer predicciones sobre otros objetos que caen, Galileo necesita números reales, en lugar de los parámetros desconocidos.
+
+#Las estimaciones de LSE squares parecen un enfoque razonable.
+
+#Entonces, ¿cómo encontramos las estimaciones de cuadrados LSE?
   
-  Tenga en cuenta que los cálculos de LSE no requieren que los errores sean aproximadamente normales.
+#Tenga en cuenta que los cálculos de LSE no requieren que los errores sean aproximadamente normales.
 
-La función lm () encontrará las betas que minimizan la suma residual de cuadrados, que es lo que queremos.
+#La función lm () encontrará las betas que minimizan la suma residual de cuadrados, que es lo que queremos.
 
-Entonces usamos este código para obtener nuestros parámetros estimados.
+#Entonces usamos este código para obtener nuestros parámetros estimados.
 
-Para verificar si la parábola estimada se ajusta a los datos, el aumento de la función de escoba () nos permite hacerlo fácilmente.
+#Para verificar si la parábola estimada se ajusta a los datos, el aumento de la función de broom() nos permite hacerlo fácilmente.
 
-Usando este código, podemos hacer la siguiente gráfica.
+#Usando este código, podemos hacer la siguiente gráfica.
 
-Tenga en cuenta que los valores predichos van directo a través de los puntos.
+#Tenga en cuenta que los valores predichos van directo a través de los puntos.
 
-Ahora, gracias a mi profesor de física de la escuela secundaria, sé que la ecuación para la trayectoria de un objeto que cae es la siguiente.
+#Ahora, gracias a mi profesor de física de la escuela secundaria, sé que la ecuación para la trayectoria de un objeto que cae es la siguiente.
 
-Con h0 y v0, la altura de inicio y la velocidad de inicio respectivamente.
+#Con h0 y v0, la altura de inicio y la velocidad de inicio respectivamente.
 
-Los datos que usamos siguen esta ecuación y agregan un error de medición para simular y observar.
+#Los datos que usamos siguen esta ecuación y agregan un error de medición para simular y observar.
 
-Dejar caer la pelota, eso significa que la velocidad de inicio es 0 porque comenzamos simplemente cayéndola desde la Torre de Pisa, que tiene una altura de aproximadamente 56.67 metros.
+#Dejar caer la pelota, eso significa que la velocidad de inicio es 0 porque comenzamos simplemente cayéndola desde la Torre de Pisa, que tiene una altura de aproximadamente 56.67 metros.
 
-Estas cantidades conocidas son consistentes con los parámetros que estimamos, que podemos ver usando la función de ordenado.
+#Estas cantidades conocidas son consistentes con los parámetros que estimamos, que podemos ver usando la función de ordenado.
 
-Aquí están.
+#Aquí están.
 
-La altura de la Torre de Pisa está dentro del intervalo de confianza para beta 0.
+#La altura de la Torre de Pisa está dentro del intervalo de confianza para beta 0.
 
-La velocidad inicial de 0 está en el intervalo de confianza para beta 1.
+#La velocidad inicial de 0 está en el intervalo de confianza para beta 1.
 
-Tenga en cuenta que el valor de p es mayor que 0.05, lo que significa que no rechazaríamos la hipótesis de que la velocidad de inicio es 0.
+#Tenga en cuenta que el valor de p es mayor que 0.05, lo que significa que no rechazaríamos la hipótesis de que la velocidad de inicio es 0.
 
-Y finalmente, la constante de aceleración está en los intervalos de confianza para 2 veces negativo beta 2.
+#Y finalmente, la constante de aceleración está en los intervalos de confianza para 2 veces negativo beta 2.
