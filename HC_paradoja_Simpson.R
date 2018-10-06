8695b4aa8388ac5f3c09722ade3224973342af8e
wparedesgt <wparedesgt@gmail.com>
2018-10-06 13:06:42 -0600

    Fin

--- HC_paradoja_Simpson.R
+++ HC_paradoja_Simpson.R
@@ -1,2 +1,25 @@
 #Paradoja de Simpson
 
+#Acabamos de ver un ejemplo de la paradoja de Simpson.
+
+#Se denomina paradoja porque vemos el signo del cambio de correlación cuando calculamos en toda la población y cuando calculamos en estratos específicos.
+
+#Ahora, vamos a utilizar un ejemplo simulado muy ilustrativo para mostrarle cómo puede suceder esto.
+
+#Supongamos que tienes tres variables: x, y y z.
+
+#Aquí hay un diagrama de dispersión de y contra x.
+
+#Puedes ver que x y y están correlacionadas negativamente.
+
+#Sin embargo, una vez que estratificamos por z el factor de confusión que no hemos visto, vemos que surge otro patrón.
+
+#Esta trama, los diferentes estratos definidos por la enfermedad, se muestran en diferentes colores.
+
+#Si calcula la correlación en cada estrato, verá que las correlaciones ahora son positivas.
+
+#Entonces, realmente es z la cual está correlacionada negativamente con x.
+
+#Si estratificamos por z, la x y la y están realmente correlacionadas positivamente.
+
+#Este es un ejemplo de la paradoja de Simpson.
